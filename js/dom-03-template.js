7e535c46584fb74ed87eefb77f5742fae47508e6
Pongsapak <tankung47@gmail.com>
2025-12-01 15:46:01 +0700

    Updates

---
+++ js/dom-03-template.js
@@ -0,0 +1,46 @@
+function creatInputComponent() {
+  const numberInputs = [...document.querySelectorAll(".app-inp-number")];
+
+  const templateElem = document.querySelector(".app-tmp-number-component");
+
+  if (templateElem) {
+    const inputContainer =
+      templateElem.content.cloneNode(true).firstElementChild;
+
+    [...inputContainer.querySelectorAll(".app-title-number")].forEach(
+      (elem) => (elem.textContent = `${numberInputs.length + 1}`)
+    );
+
+    const inputElem = inputContainer.querySelector(".app-inp-number");
+
+    inputElem?.addEventListener("change", () => {
+      const numberInputs = [...document.querySelectorAll(".app-inp-number")];
+
+      const result = numberInputs.reduce(
+        (result, elem) =>
+          result + (Number.isNaN(elem.valueAsNumber) ? 0 : elem.valueAsNumber),
+        0
+      );
+
+      const numberOutput = document.querySelectorAll(".app-out-number");
+      numberOutput.forEach((elem) => (elem.textContent = result));
+    });
+
+    const container = document.querySelector(".app-cmp-number-list");
+    if (container) {
+      container.append(inputContainer);
+    }
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const addButtons = [...document.querySelectorAll(".app-cmp-number-input")];
+
+  addButtons.forEach((elem) =>
+    elem.addEventListener("click", () => {
+      creatInputComponent();
+    })
+  );
+
+  creatInputComponent();
+});
