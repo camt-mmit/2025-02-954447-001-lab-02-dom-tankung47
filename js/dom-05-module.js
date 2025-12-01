bea631ec7c934529c20bcfd787b8b3798016de2e
Pongsapak <tankung47@gmail.com>
2025-12-01 16:05:38 +0700

    Updates

--- js/dom-05-module.js
+++ js/dom-05-module.js
@@ -1,3 +1,3 @@
 import { createComponent as createInputListComponent } from "./input-list-component.js";
 
-createInputComponent(document.querySelector("body"));
+createInputListComponent(document.querySelector("body"));
